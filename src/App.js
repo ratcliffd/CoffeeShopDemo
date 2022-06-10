2593079e6f3c99bf031909606d3f7608e23f0ed5
Derek Ratcliff <derekaratcliff@gmail.com>
2022-06-10 16:38:35 -0500

    created first components for pages and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './components/Home/HomePage';
 
 
 function App() {
   return (
     <React.Fragment>
-      
+      <HomePage/>
     </React.Fragment>
   );
 }
